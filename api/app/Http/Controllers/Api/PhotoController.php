0f2472868a7e656ad798f422ce4365c873aaf6f9
gaetanccg <gaetan6942@gmail.com>
2026-02-03 00:16:20 +0100

    fix pint

--- api/app/Http/Controllers/Api/PhotoController.php
+++ api/app/Http/Controllers/Api/PhotoController.php
@@ -13,7 +13,6 @@ use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Storage;
-use Illuminate\Support\Str;
 
 class PhotoController extends Controller
 {
